635417b73d0faf69ef5c7f0e40538f958109726c
Inixio <inixio97@gmail.com>
2021-07-12 20:32:06 +0200

    ADD secret

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   client_id: '71240b2346fe478ab8600e434c60246f',
   client_secret: '9823ec54d2f4407a9188ed9361114d1a',
-  redirect_url: 'https://spotivision.herokuapp.com/'
+  redirect_url: 'https://spotivisionapp.herokuapp.com/'
 }
